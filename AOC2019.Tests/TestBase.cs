a0984dea2e250cace2cabff8be40131e2acd9bdb
Tony Hunt <TechnoTone@gmail.com>
2020-07-06 23:51:24 +0100

    Day01

---
+++ AOC2019.Tests/TestBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC2019.Tests
+{
+    public class TestBase
+    {
+        internal readonly InputReader input;
+
+        internal class InputReader
+        {
+            private const string INPUT_PATH = "../../../../input";
+            private readonly int day;
+
+            public InputReader(int day)
+            {
+                this.day = day;
+            }
+
+            private IEnumerable<string> asLines() =>
+                File.ReadAllLines($"{INPUT_PATH}/day{day:00}.txt");
+
+            internal List<string> asList() => 
+                asLines().ToList();
+
+            internal List<int> asListOfInts() =>
+                asList().ConvertAll(int.Parse);
+        }
+
+        protected TestBase(int day)
+        {
+            input = new InputReader(day);
+        }
+    }
+}
---
+++ AOC2019/Day01.cs
@@ -0,0 +1,19 @@
+﻿namespace AOC2019
+{
+    public static class Day01
+    {
+        public static int fuelFor(int mass) =>
+            mass / 3 - 2;
+
+
+        public static int correctFuelFor(int mass)
+        {
+            if (mass <= 0)
+                return 0;
+            
+            var x = fuelFor(mass);
+            return x > 0 ? x + correctFuelFor(x) : 0;
+        }
+        
+    }
+}
